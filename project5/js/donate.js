455905934f0b884c731bff353fc39ae824b1f25a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-03 21:34:34 +0600

    ** step 1 receive value done

---
+++ project5/js/donate.js
@@ -0,0 +1,6 @@
+// step 1 : value is receive 
+document.getElementById('donate1btn').addEventListener('click',function(e){
+    const donate1Value  = getInputValue('donate1');
+    console.log('donate 1 is :',donate1Value);
+    // step 2  add donate + and - main bvalance---
+})
--- project5/js/utilities.js
+++ project5/js/utilities.js
@@ -1,7 +1,13 @@
-
+// page change on click
 function blogPage() {
     window.location.href = '/Blog.html';
 }
 function goToHomepage() {
-    window.location.href = '/index.html'; // Replace with your homepage URL
+    window.location.href = '/index.html'; 
+}
+// Donate page function
+function getInputValue(id){
+    const value = document.getElementById(id).value;
+    const numberValue = parseFloat(value);
+    return(numberValue);
 }
