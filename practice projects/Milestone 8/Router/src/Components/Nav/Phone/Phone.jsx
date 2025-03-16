a5441dfd443efcd8e3f6fc98797c3189e99ff0d3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-03-16 23:17:12 +0600

    * Module  41 Complete *

--- practice projects/Milestone 8/Router/src/Components/Nav/Phone/Phone.jsx
+++ practice projects/Milestone 8/Router/src/Components/Nav/Phone/Phone.jsx
@@ -1,25 +1,41 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Audio } from 'react-loader-spinner';
 
 const Phone = () => {
-    const [phone,setPhone]=useState([]);
+  const [phone, setPhone] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-useEffect(()=>{
+  useEffect(() => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        setTimeout(() => {
+          setPhone(data.data.data);
+          setLoading(false);
+        }, 1000); 
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
 
-
-    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(data =>setPhone(data.data.data))
-
-
-
-    
-},[]);
-
-    return (
-        <div>
-            <h2 className="text-5xl">Phone : {phone.length}</h2>
-        </div>
-    );
+  return (
+    <div>
+      {loading ? (
+        <Audio
+          height="100"
+          width="100"
+          color="#4fa94d"
+          ariaLabel="audio-loading"
+          visible={true}
+        />
+      ) : (
+        <h2 className="text-5xl">Phone : {phone.length}</h2>
+      )}
+    </div>
+  );
 };
 
 export default Phone;
