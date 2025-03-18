e9222faaa83da786d68984121a1f0e4c498a35f2
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-03-18 15:34:36 +0600

    *Active NavLink*

--- practice projects/Milestone 8/introduction-to-react-router/src/Header.jsx
+++ practice projects/Milestone 8/introduction-to-react-router/src/Header.jsx
@@ -1,15 +1,15 @@
 
-import { Link } from 'react-router-dom';
+import { Link,NavLink } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
         <div>
                 <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/User">User</Link>
-                    <Link to="/posts">Posts</Link>
-                    <Link to="/contact">Contact</Link>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/User">User</NavLink>
+                    <NavLink to="/posts">Posts</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
     
             </nav>
         </div>
