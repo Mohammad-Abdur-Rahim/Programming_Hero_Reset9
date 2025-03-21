bce493dda4221027c58e563d5a9a1805c9dfd3fc
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-03-21 21:53:54 +0600

    * Project Setup Done *

---
+++ practice projects/Milestone 8/book_Vibe_project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="text-4xl text-emerald-700 font-bold text-center ">
+        <h2>Book Vibe Project  </h2>
+       
+      </div>
+    </>
+  );
+}
+
+export default App;
