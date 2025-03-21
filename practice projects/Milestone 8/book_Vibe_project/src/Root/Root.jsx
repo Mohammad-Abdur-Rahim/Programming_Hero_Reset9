1aabcc8350dc930b6d4ee18858adf2864bd84dfe
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-03-21 22:12:05 +0600

    * Router setup 1 *

---
+++ practice projects/Milestone 8/book_Vibe_project/src/Root/Root.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Nav from "../Navbar/Nav";
+
+
+const Root = () => {
+    return (
+        <div>
+           <Nav></Nav>
+           <Outlet></Outlet>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
